1b71b511926d95ff42b07536b3a94d17a8eb7d12
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-03 12:00:15 +0530

    Add files via upload

---
+++ frontend/Footer.jsx
@@ -0,0 +1,6 @@
+<div className="text-center mt-6 text-sm text-gray-500">
+  <p>© 2026 ShopEZ. All rights reserved.</p>
+  <p className="mt-2">Developed by Team ShopEZ</p>
+<p className="mt-1">Team Lead: Ambikesh Dwivedi</p>
+  <p className="mt-1">Members: Anushka, Arvind</p>
+</div>
